4688c20bf07c4bdacd998844e02791b3f9270517
Szymon Oleszek <szymqwerty@gmail.com>
2024-01-28 15:03:46 +0100

    onemoretime

--- src/index.js
+++ src/index.js
@@ -3,21 +3,13 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import { BrowserRouter } from "react-router-dom";
 
-import { Provider } from "react-redux";
-// krok 3 import nowostworzonego stora
-import { store } from "./store";
-
 import "@picocss/pico";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      {/* krok 1 - setup provider reduxowego */}
-      {/* krok 4 - przekazanie zaimportowanego stora jako props  */}
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
--- src/pages/Exercises/Counter/Counter.js
+++ src/pages/Exercises/Counter/Counter.js
@@ -1,41 +1,13 @@
 import React from "react";
-import { increment } from "../../../redux/CounterSlice";
-
-import { useSelector, useDispatch } from "react-redux";
 
 export const Counter = () => {
-  // CounterSlice.js
-  // const counterSlice = createSlice({
-  //   initialState: {
-  //     value: 0,
-  //     trelemorele: []
-  //   },
-
-  // const tablicaTrelemorele =  useSelector((state)=>state.counter.trelemorele);
-  // możemy wyciągnąć cały obiekt counter jeżeli potrzebujemy
-  // const całyStanCountera = useSelector((state) => state.counter);
-
-  const counter = useSelector((state) => state.counter.value); // słowo value dlatego że nazwa klucza w tym kawałku stanu też jest słowem value
-
-  // useDispatch zwraca funckje która pozwala dispatchować akcje stworzne w slice-ach
-  const dispatch = useDispatch();
-
   return (
     <article>
       <h2>Counter</h2>
       <p>
-        Wartość countera: <b>{counter}</b>
+        Wartość countera: <b>{2}</b>
       </p>
-      <button
-        onClick={() => {
-          /**
-           * Dispatch akcji increment
-           */
-          dispatch(increment());
-        }}
-      >
-        +1
-      </button>
+      <button>+1</button>
     </article>
   );
 };
